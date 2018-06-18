4f96eb411e047e4c7f513f14b3290b89c4c25fd8
Anh B. Tran <anhb.tran1010@gmail.com>
2018-06-18 15:04:40 -0500

    User can change grid dimensions

--- java/com/example/anhbt/hpcgbenchmark/MainActivity.java
+++ java/com/example/anhbt/hpcgbenchmark/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.anhbt.hpcgbenchmark;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int nx = 10;
-    int ny = 10;
-    int nz = 10;
+    double result;
     char[] dimensions;
-    //EditText xDim = (EditText)findViewById(R.id.editText);
-    //EditText yDim = (EditText)findViewById(R.id.editText2);
+
+    EditText resultNumber;
+    EditText xDim;
+    EditText yDim;
+    EditText zDim;
+    TextView testResult;
     //EditText zDim = (EditText)findViewById(R.id.editText3);
     // Used to load the 'native-lib' library on application startup.
+    Button runButton;
     static {
         System.loadLibrary("native-main");
     }
@@ -24,17 +29,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runButton = findViewById(R.id.button);
+        xDim = findViewById(R.id.editText);
+        yDim = findViewById(R.id.editText2);
+        zDim = findViewById(R.id.editText3);
+        resultNumber = findViewById(R.id.result);
+        testResult = findViewById(R.id.editText4);
 
-        //xDim.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-          //  @Override
-            //public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-              //  boolean handled =  false;
-                //if(actionId == EditorInfo.IME_ACTION_NEXT){
-                  //  nx =Integer.parseInt( xDim.getText().toString());
-                //}
-                //return handled;
-            //}
-        //});
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nx;
+                String ny;
+                String nz;
+                nx = String.valueOf(xDim.getText());
+                ny = String.valueOf(yDim.getText());
+                nz = String.valueOf(zDim.getText());
+                result =  main(4,nx,ny,nz);
+                resultNumber.setText(Double.toString(result));
+                resultNumber.setVisibility(View.VISIBLE);
+                testResult.setVisibility(View.VISIBLE);
+            }
+        });
+
 
         //yDim.setOnEditorActionListener(new TextView.OnEditorActionListener() {
           //  @Override
@@ -58,11 +75,6 @@ public class MainActivity extends AppCompatActivity {
             //}
         //});
 
-
-        int result =  main(1, '0');
-        System.out.print(result);
-        //String test = stringFromJNI();
-
     }
 
     /**
@@ -70,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
 
-    public native int main(int argc, char test);
+    public native double main(int argc, String nx, String ny, String nz);
 
     //public native String stringFromJNI();
 }
