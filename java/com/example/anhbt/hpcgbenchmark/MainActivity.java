d4d55502ce0d5d1a6601bf89c67f440648a405a6
Anh B. Tran <anhb.tran1010@gmail.com>
2018-07-06 12:24:12 -0500

    Serial version for android

--- java/com/example/anhbt/hpcgbenchmark/MainActivity.java
+++ java/com/example/anhbt/hpcgbenchmark/MainActivity.java
@@ -2,22 +2,25 @@ package com.example.anhbt.hpcgbenchmark;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-    double result;
+    double[] result;
     char[] dimensions;
 
-    EditText resultNumber;
+    TextView resultNumber;
     EditText xDim;
     EditText yDim;
     EditText zDim;
     TextView testResult;
+    TextView BandWidth;
+    TextView BwResult;
     //EditText zDim = (EditText)findViewById(R.id.editText3);
     // Used to load the 'native-lib' library on application startup.
     Button runButton;
@@ -33,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
         xDim = findViewById(R.id.editText);
         yDim = findViewById(R.id.editText2);
         zDim = findViewById(R.id.editText3);
-        resultNumber = findViewById(R.id.result);
+        resultNumber = findViewById(R.id.Result);
         testResult = findViewById(R.id.editText4);
+        BandWidth = findViewById(R.id.BandWidth);
+        BwResult = findViewById(R.id.BwResult);
 
         runButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,9 +51,21 @@ public class MainActivity extends AppCompatActivity {
                 ny = String.valueOf(yDim.getText());
                 nz = String.valueOf(zDim.getText());
                 result =  main(4,nx,ny,nz);
-                resultNumber.setText(Double.toString(result));
-                resultNumber.setVisibility(View.VISIBLE);
-                testResult.setVisibility(View.VISIBLE);
+                if(result[0]==0){
+
+                }
+                else {
+                    DecimalFormat df = new DecimalFormat("#.#####");
+                    df.setRoundingMode(RoundingMode.CEILING);
+                    double gflops = result[0];
+                    double bandwidth = result[1];
+                    resultNumber.setText(df.format(gflops));
+                    BwResult.setText(Double.toString(bandwidth));
+                    resultNumber.setVisibility(View.VISIBLE);
+                    testResult.setVisibility(View.VISIBLE);
+                    BandWidth.setVisibility(View.VISIBLE);
+                    BwResult.setVisibility(View.VISIBLE);
+                }
             }
         });
 
@@ -82,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
 
-    public native double main(int argc, String nx, String ny, String nz);
+    public native double[] main(int argc, String nx, String ny, String nz);
 
     //public native String stringFromJNI();
 }
