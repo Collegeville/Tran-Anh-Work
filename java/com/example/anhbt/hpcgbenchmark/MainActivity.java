483782c0703de6cde1f2669fb0f6c9f6c19f928d
Anh B. Tran <anhb.tran1010@gmail.com>
2018-07-12 12:31:40 -0500

    Multi threaded version
    This version included the back ground thread and the UI thread. As a result, it run smoother and there will be no frozen screen while waiting for the program to run.

--- java/com/example/anhbt/hpcgbenchmark/MainActivity.java
+++ java/com/example/anhbt/hpcgbenchmark/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.anhbt.hpcgbenchmark;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.math.RoundingMode;
@@ -21,8 +24,7 @@ public class MainActivity extends AppCompatActivity {
     TextView testResult;
     TextView BandWidth;
     TextView BwResult;
-    //EditText zDim = (EditText)findViewById(R.id.editText3);
-    // Used to load the 'native-lib' library on application startup.
+    ProgressBar loading;
     Button runButton;
     static {
         System.loadLibrary("native-main");
@@ -40,66 +42,64 @@ public class MainActivity extends AppCompatActivity {
         testResult = findViewById(R.id.editText4);
         BandWidth = findViewById(R.id.BandWidth);
         BwResult = findViewById(R.id.BwResult);
+        loading = findViewById(R.id.progressBar);
 
         runButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                resultNumber.setVisibility(View.INVISIBLE);
+                testResult.setVisibility(View.INVISIBLE);
+                BandWidth.setVisibility(View.INVISIBLE);
+                BwResult.setVisibility(View.INVISIBLE);
+                loading.setVisibility(View.VISIBLE);
                 String nx;
                 String ny;
                 String nz;
                 nx = String.valueOf(xDim.getText());
                 ny = String.valueOf(yDim.getText());
                 nz = String.valueOf(zDim.getText());
-                result =  main(4,nx,ny,nz);
-                if(result[0]==0){
+                hpcg runTask = new hpcg();
+                runTask.execute(nx,ny,nz);
 
-                }
-                else {
-                    DecimalFormat df = new DecimalFormat("#.#####");
-                    df.setRoundingMode(RoundingMode.CEILING);
-                    double gflops = result[0];
-                    double bandwidth = result[1];
-                    resultNumber.setText(df.format(gflops));
-                    BwResult.setText(Double.toString(bandwidth));
-                    resultNumber.setVisibility(View.VISIBLE);
-                    testResult.setVisibility(View.VISIBLE);
-                    BandWidth.setVisibility(View.VISIBLE);
-                    BwResult.setVisibility(View.VISIBLE);
-                }
             }
         });
 
-
-        //yDim.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-          //  @Override
-            //public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-              //  boolean handled =  false;
-                //if(actionId == EditorInfo.IME_ACTION_NEXT){
-                  //  nx =Integer.parseInt( xDim.getText().toString());
-                //}
-                //return handled;
-            //}
-        //});
-
-        //zDim.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-          //  @Override
-            //public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-              //  boolean handled =  false;
-                //if(actionId == EditorInfo.IME_ACTION_NEXT){
-                  //  nx =Integer.parseInt( xDim.getText().toString());
-                //}
-                //return handled;
-            //}
-        //});
-
     }
+    private class hpcg extends AsyncTask<String, Void, double[]> {
+        double[] result;
 
-    /**
-     * A native method that is implemented by the 'native-main' native library,
-     * which is packaged with this application.
-     */
+        //String nx, ny, nz;
+        //public hpcg(String nx, String ny, String nz){
+        //this.nx = nx;
+        //this.ny = ny;
+        //this.nz = nz;
+        //}
+        @Override
+        protected double[] doInBackground(String...strings) {
+            result =  main(4,strings[0],strings[1],strings[2]);
+            if(result[0]==0){
+                return null;
+            }
+            else{
+                return result;
+            }
+        }
 
+
+        @Override
+        protected void onPostExecute(double[] finalResult){
+                loading.setVisibility(View.INVISIBLE);
+                DecimalFormat df = new DecimalFormat("#.#####");
+                df.setRoundingMode(RoundingMode.CEILING);
+                double gflops = finalResult[0];
+                double bandwidth = finalResult[1];
+                resultNumber.setText(df.format(gflops));
+                BwResult.setText(Double.toString(bandwidth));
+                resultNumber.setVisibility(View.VISIBLE);
+                testResult.setVisibility(View.VISIBLE);
+                BandWidth.setVisibility(View.VISIBLE);
+                BwResult.setVisibility(View.VISIBLE);
+        }
+    }
     public native double[] main(int argc, String nx, String ny, String nz);
-
-    //public native String stringFromJNI();
 }
